9349914b556f08f2e7d9b23840503cf4d8792d5a
Dylan Ireland <dylan@casper.network>
2024-02-26 16:17:21 -0500

    updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,50 @@
 function relocate(loc) {
-	window.scroll({
-		top: 0,
-		left: 0,
-		behavior: "smooth"
-	});
+	if (loc === "/") {
+		window.scroll({
+			top: 0,
+			left: 0,
+			behavior: "smooth"
+		});
+	} else if (loc.startsWith("#")) {
+		const element = document.querySelector(loc);
+		if (element) {
+			element.scrollIntoView({
+				behavior: "smooth"
+			});
+		}
+	}
+}
+
+function openLink(project) {
+	let location = "about:blank";
+	switch (project) {
+		case "balloon":
+			location = "https://balloon.trading";
+			break;
+		case "jukebox":
+			location = "https://jukebox.wtf";
+			break;
+		case "dappdev":
+			location =
+				"https://youtube.com/playlist?list=PL8oWxbJ-csEqjX2A1pprKuHzCBS8RMcmR&si=9ux-QOpjUKAJDame";
+			break;
+		case "aero":
+			location = "https://pittsburghaero.com";
+			break;
+		case "github":
+			location = "https://github.com/dylanireland";
+			break;
+		case "linkedin":
+			location = "https://linkedin.com/in/dylanireland/";
+			break;
+		case "medium":
+			location = "https://medium.com/@dylan.ireland777";
+			break;
+		case "x":
+			location = "https://twitter.com/DylanIrel";
+			break;
+		default:
+			break;
+	}
+	window.open(location, "_blank");
 }
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,14 +2,13 @@ const navbar = document.getElementById("nav");
 
 function shrinkNavbar(scrollTop) {
 	const navbar = document.getElementById("nav");
-	console.log(scrollTop);
 
 	navbar.style.width = "calc(50% - " + (scrollTop * 2).toString() + "px)";
-	navbar.style.left = "calc(25% - " + scrollTop.toString() + "px)";
+	navbar.style.left = "calc(25% + 0.5rem - " + scrollTop.toString() + "px)";
 
 	if (navbar.offsetWidth <= navbar.offsetHeight) {
 		navbar.style.width = "calc(" + navbar.offsetHeight.toString() + "px - 2rem";
-		navbar.style.left = "1rem";
+		navbar.style.left = "2rem";
 		return;
 	}
 }
