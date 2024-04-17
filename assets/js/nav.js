c87564af58239e4be3ce64fb5b0e164b428c47f3
Dylan Ireland <dylan@casper.network>
2024-04-17 16:00:17 -0400

    responsive, mobile css

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,13 +1,31 @@
 const navbar = document.getElementById("nav");
 
-function shrinkNavbar(scrollTop) {
+function shrinkNavbar(scrollTop, navbarWidth) {
 	const navbar = document.getElementById("nav");
 
-	navbar.style.width = "calc(50% - " + (scrollTop * 2).toString() + "px)";
-	navbar.style.left = "calc(25% + 0.5rem - " + scrollTop.toString() + "px)";
+	const navPadding = window
+		.getComputedStyle(navbar)
+		.getPropertyValue("padding");
+
+	navbar.style.width = `calc(${navbarWidth}% - ${(
+		scrollTop * 2
+	).toString()}px)`;
+	const leftPercentage = (100 - navbarWidth) / 2;
+	navbar.style.left = `calc(${leftPercentage}% - ${scrollTop.toString()}px)`;
+	const distanceLeft = window
+		.getComputedStyle(navbar)
+		.getPropertyValue("left")
+		.slice(0, -2);
+	const remSize = window
+		.getComputedStyle(navbar)
+		.getPropertyValue("font-size")
+		.slice(0, -2);
+	if (distanceLeft < remSize * 2) {
+		navbar.style.left = "2rem";
+	}
 
 	if (navbar.offsetWidth <= navbar.offsetHeight) {
-		navbar.style.width = "calc(" + navbar.offsetHeight.toString() + "px - 2rem";
+		navbar.style.width = `calc(${navbar.offsetHeight.toString()}px - 2rem`;
 		navbar.style.left = "2rem";
 		return;
 	}
@@ -26,5 +44,18 @@ window.onscroll = () => {
 };
 
 function scrollingFunctions() {
-	shrinkNavbar(window.scrollY || window.pageYOffSet || 0);
+	const navbarWidth = getNavbarWidth();
+	shrinkNavbar(window.scrollY || window.pageYOffSet || 0, navbarWidth);
+}
+
+function getNavbarWidth() {
+	const ratio = window.innerWidth / window.innerHeight;
+	console.log(ratio);
+	let width = 50;
+	if (ratio < 3 / 4) {
+		width = 80;
+	} else if (ratio < 1) {
+		width = 70;
+	}
+	return width;
 }
